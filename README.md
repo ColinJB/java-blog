c8220542e6064095efacb2ac97f6023df11b27cb
Alex Lee and Colin Bloom <colin@>
2017-04-05 11:00:29 -0700

    add initial project directories and files

---
+++ README.md
@@ -0,0 +1,29 @@
+# _javaBlog_
+
+#### _javaBlog, 04-05-2017_
+
+#### By _**Alex Lee and Colin Bloom**_
+
+## Description
+_Example text for the description of the project_
+
+
+## Specifications
+
+| Behavior                   | Input Example     | Output Example    |
+| -------------------------- | -----------------:| -----------------:|
+
+
+
+## Setup/Installation Requirements
+
+* _Clone the repository_
+* _Run the command 'gradle run'_
+* _Open browser and go to localhost:4567_
+
+
+### License
+
+Copyright (c) 2017 **_Alex Lee and Colin Bloom_**
+
+This software is licensed under the MIT license.
---
+++ src/test/java/DatabaseRule.java
@@ -0,0 +1,26 @@
+import org.junit.rules.ExternalResource;
+import org.sql2o.*;
+
+public class DatabaseRule extends ExternalResource {
+
+  @Override
+  protected void before() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/java_blog_test", null, null);
+  }
+
+  @Override
+  protected void after() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteSubmissionQuery = "DELETE FROM submissions *;";
+      con.createQuery(deleteSubmissionQuery).executeUpdate();
+      String deleteCommentQuery = "DELETE FROM comments *;";
+      con.createQuery(deleteCommentQuery).executeUpdate();
+      String deletePostQuery = "DELETE FROM posts *;";
+      con.createQuery(deletePostQuery).executeUpdate();
+      String deleteTagQuery = "DELETE FROM tags *;";
+      con.createQuery(deleteTagQuery).executeUpdate();
+      String deleteUserQuery = "DELETE FROM users *;";
+      con.createQuery(deleteUserQuery).executeUpdate();
+    }
+  }
+}
